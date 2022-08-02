bfa5f96e3cfa6fbc2b2e081d793a298a6d9bcdf2
Simona Zatková <sisizatkova330@gmail.com>
2022-08-02 10:16:52 +0200

    updateovanie

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("JavaScript funguje!");
+
+let visualniHlavniOdstavec=document.querySelector(".hlavni-odstavec");
+visualniHlavniOdstavec.textContent="Domovska stranka";
+
+let visualnimojefoto = document.querySelector(".moje-foto");
+visualnimojefoto.src="img/druhe-foto.jpeg";
+
+let visualnivzdelani=document.querySelector(".vzdelani");
+visualnivzdelani.textContent="Skoly";
+
+let visualniprednastavenise=document.querySelector(".predstaveni-se");
+visualniprednastavenise.style.color = "#0000FF";
+
+function pridejzvyrazneni(){
+    let visualnitlacitko1=document.querySelector(".tlacitko1");
+    visualnitlacitko1.classList.add("zvyrazneny");
+}
